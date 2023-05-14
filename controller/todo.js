1aa8de90600bb1f546adaa3a19fb1f7d756c53b4
sali-ayse <aysesali34@gmail.com>
2023-05-14 21:48:07 +0200

    CSS pretty and other local storage functions

--- controller/todo.js
+++ controller/todo.js
@@ -36,11 +36,55 @@ const addTodoToLocalStorage = (todo) => {
 }
 
 const editTodo = (e) => {
-  e.preventDefault();
+  if (e.target.className === "todo-edit") {
+    const id = e.target.parentElement.classList[0];
+    const todoText = e.target.parentElement.querySelector(".todo-text");
+
+    if (todoText.disabled) {
+      todoText.disabled = false;
+      e.target.textContent = "Save";
+    } else {
+      todoText.disabled = true;
+      e.target.textContent = "Edit";
+      editTodoInLocalStorage(id, todoText.value);
+    }
+  }
 }
 
-const checkTodo = (e) => { //44:34
-  e.preventDefault();
+const editTodoInLocalStorage = (id, todo) => {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+
+  tasks = tasks.map(task => {
+    if (task.id === parseInt(id)) {
+      task.todo = todo;
+    }
+    return task;
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+const checkTodo = (e) => {
+  if (e.target.className === "todo-check") {
+    const id = e.target.parentElement.classList[0];
+
+    const checked = e.target.checked;
+
+    if (checked) {
+      e.target.nextElementSibling.style.textDecoration = "line-through";
+    } else {
+      e.target.nextElementSibling.style.textDecoration = "none";
+    }
+
+    let tasks = JSON.parse(localStorage.getItem(tasks));
+
+    tasks = tasks.map(task => {
+      if (task.id === parseInt(id)) {
+        task.completed = checked;
+      }
+      return addTodoToLocalStorage;
+    });
+    localStorage.setItem("tasks", JSON.stringify);
+  }
 }
 
 const deleteTodo = (e) => {
@@ -52,7 +96,9 @@ const deleteTodo = (e) => {
 }
 
 const deleteTodoFromLocalStorage = (id) => {
-
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  tasks = tasks.filter((task) => task.id !== parseInt(id));
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 const getTodo = (e) => {
@@ -65,7 +111,7 @@ const getTodo = (e) => {
       todoItem.className = `todo-item ${id}`;
       todoItem.innerHTML = `
       <input type="checkbox" class="todo-check" ${task.completed ? "checked" : ""}>
-      <input type="text" class="todo-text" ${task.completed ? "strike" : ""} value=${todo} disabled>
+      <input type="text" class="todo-text" ${task.completed ? "strike" : ""} value=${task.todo} disabled>
       <button class="todo-edit">Edit</button>
       <button class="todo-delete">Delete</button>`;
       todoOutput.appendChild(todoItem);
